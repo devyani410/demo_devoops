f7c88c5197e7ec386ffab9584f77e1b032838ae3
Devyani kumrawat <70192245+devyani410@users.noreply.github.com>
2022-12-22 23:38:23 +0530

    this is my first commit

---
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -0,0 +1,20 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Demo1Application {
+    @GetMapping("/hello")
+	public String get(){
+		return "hello user !!!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+}
